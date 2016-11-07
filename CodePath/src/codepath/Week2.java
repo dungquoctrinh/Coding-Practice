2028d29d47cb518a6fda81e8ce0575b31935aedf
Dung (Jun) Trinh <dungtrinh1993@gmail.com>
2016-11-07 04:23:43 -0800

    add the file for week 1 and 2. Implement the code as well

--- CodePath/src/codepath/Week2.java
+++ CodePath/src/codepath/Week2.java
@@ -28,12 +28,37 @@ public class Week2 {
             val = node;
             next = null;
         }
+        
+        /**
+         * Add value using node
+         * @param node
+         * @return 
+         */
+        public boolean addLinkedList(LinkedListNode node) {
+            if (node == null) {
+                return false;
+            }
+            this.next = node;
+            //add node in here
+            return true;
+        } 
+        
+        /**
+         * Add value using a value
+         * @param value
+         * @return 
+         */
+        public boolean addLinkedList(int value) {
+            this.next = new LinkedListNode(value);
+        }
     }
-    /*
-    * Complete the function below.
-    */
 
-
+    /**
+     * Remove the node that is greater than value x
+     * @param list
+     * @param x
+     * @return 
+     */
     static LinkedListNode removeNodes(LinkedListNode list, int x) {
         if (list == null) {
             return null;
@@ -58,7 +83,11 @@ public class Week2 {
 
     }
     
-    
+    /**
+     * Detect the first repeated word in a sentence
+     * @param s String to detect
+     * @return the repeated word
+     */
     static String firstRepeatedWord(String s) {
         String newString = "";
         Set stringSet  = new HashSet();
